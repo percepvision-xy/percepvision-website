79aced189dd9f6f38836760c733558e2ea80adfd
batteryspecial <infinityscrub74@gmail.com>
2026-02-08 01:14:34 -0500

    nit: expanded get started click region to entire button

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -48,14 +48,14 @@ export function Navbar() {
           >
             Log in
           </button>
-          <button
-            type="button"
-            className="text-white bg-[#0072FF] hover:bg-[#003699] focus:outline-none font-medium rounded-full text-sm px-6 py-2.5 text-center transition-all shadow-lg shadow-[#0072FF]/30"
-          >
-            <Link href="/contact">
+          <Link href="/contact">
+            <button
+              type="button"
+              className="text-white bg-[#0072FF] hover:bg-[#003699] focus:outline-none font-medium rounded-full text-sm px-6 py-2.5 text-center transition-all shadow-lg shadow-[#0072FF]/30"
+            >
               Get Started
-            </Link>
-          </button>
+            </button>
+          </Link>
           <button
             type="button"
             className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-[#5c5c5c] rounded-lg md:hidden hover:bg-[#f5f5f5] focus:outline-none"
